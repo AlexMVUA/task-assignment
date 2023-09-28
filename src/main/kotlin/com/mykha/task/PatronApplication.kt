9bb7b0d53def6c3ae005fb87a09a6a02c9f6da54
Oleksii Mykhailov <alexei.mykhaylov@gmail.com>
2023-09-28 11:07:44 +0200

    Initial commit

---
+++ src/main/kotlin/com/mykha/task/PatronApplication.kt
@@ -0,0 +1,13 @@
+package com.mykha.task
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatronApplication
+
+fun main(args: Array<String>) {
+	runApplication<PatronApplication>(*args) {
+
+	}
+}
